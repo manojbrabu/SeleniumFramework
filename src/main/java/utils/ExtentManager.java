3978a3dc11656f4cd328accce037e27c07dbd96b
manojbrabu <manojbrabu@gmail.com>
2026-02-21 08:57:26 +0530

    Updated the report Path

--- src/main/java/utils/ExtentManager.java
+++ src/main/java/utils/ExtentManager.java
@@ -14,7 +14,7 @@ public class ExtentManager {
         if (extent == null) {
             ExtentSparkReporter reporter =
                     new ExtentSparkReporter(System.getProperty("user.dir")
-                            + "/test-output/ExtentReport.html");
+                            + "/Extend_20Report/ExtentReport.html");
             reporter.config().setReportName("Automation Test Report");
             reporter.config().setDocumentTitle("Extent Report");
             System.out.println("Extent report will be generated at: "
