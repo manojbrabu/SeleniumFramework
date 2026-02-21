df7e4132265b6089970bce27f601835d241b155d
manojbrabu <manojbrabu@gmail.com>
2026-02-21 07:45:41 +0530

    Updated the Base Test and All FIles as per IntelIJ

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -14,11 +14,19 @@ public class BaseTest {
     public static WebDriver driver;
     @BeforeSuite
     public void setUp() {
+        System.out.println("Started");
         String browser = ConfigReader.getProperty("browser");
+        System.out.println("Browser value: " + browser);
         if (browser.equalsIgnoreCase("firefox")) {
             WebDriverManager.firefoxdriver().setup();
             driver = new FirefoxDriver();
+        } else if (browser.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        } else {
+            throw new RuntimeException("Browser not supported: " + browser);
         }
+
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get(ConfigReader.getProperty("url"));
