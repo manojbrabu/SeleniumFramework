2a0163a27e0ef77355cb502bd431df3704bfb7f8
manojbrabu <manojbrabu@gmail.com>
2026-03-02 09:08:40 +0530

    Updated the Base Test

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -4,32 +4,32 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.*;
 import utils.ConfigReader;
 
 import java.time.Duration;
 
 public class BaseTest {
-
     private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
     public static WebDriver getDriver() {
         return driver.get();
     }
 
+    @Parameters("browser")
     @BeforeTest(alwaysRun = true)
-    public void setUp() {
-
+    public void setUp(String browser) {
         if (driver.get() == null) {
-
-            String browser = ConfigReader.getProperty("browser");
-
             if (browser.equalsIgnoreCase("chrome")) {
                 WebDriverManager.chromedriver().setup();
                 driver.set(new ChromeDriver());
-            } else {
+            } else if (browser.equalsIgnoreCase("firefox")) {
+                FirefoxOptions options = new FirefoxOptions();
+                options.addArguments("headless");
                 WebDriverManager.firefoxdriver().setup();
                 driver.set(new FirefoxDriver());
+
             }
 
             getDriver().manage().window().maximize();
@@ -40,7 +40,6 @@ public class BaseTest {
 
     @AfterTest(alwaysRun = true)
     public void tearDown() {
-
         if (driver.get() != null) {
             getDriver().quit();
             driver.remove();
--- src/main/java/utils/WaitUtils.java
+++ src/main/java/utils/WaitUtils.java
@@ -3,9 +3,12 @@ package utils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.NoSuchElementException;
 
 public class WaitUtils {
     public static WebElement waitForElement(WebDriver driver, WebElement
@@ -27,5 +30,11 @@ public class WaitUtils {
     public static void waitForElementTextPresent(WebDriver driver, WebElement element, String vText) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
         wait.until(ExpectedConditions.textToBePresentInElement(element,vText));
+    }
+    public static void fluentWait(WebDriver driver, WebElement element){
+        Wait <WebDriver> wait = new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(10))
+                .pollingEvery(Duration.ofSeconds(3))
+                .ignoring(NoSuchElementException.class);
     }
 }
