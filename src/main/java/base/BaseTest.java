b6d91896e4c3496c42a815139bd786d395ca54ec
manojbrabu <manojbrabu@gmail.com>
2026-03-04 13:52:36 +0530

    Addded Remote Webdriver

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -1,17 +1,18 @@
 package base;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.*;
 import utils.ConfigReader;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 
 public class BaseTest {
+
     private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
     public static WebDriver getDriver() {
@@ -20,26 +21,30 @@ public class BaseTest {
 
     @Parameters("browser")
     @BeforeTest(alwaysRun = true)
-    public void setUp(String browser) {
-        if (driver.get() == null) {
-            if (browser.equalsIgnoreCase("chrome")) {
-                ChromeOptions options = new ChromeOptions();
-                options.addArguments("--headless");
-                WebDriverManager.chromedriver().setup();
-                driver.set(new ChromeDriver());
-            } else if (browser.equalsIgnoreCase("firefox")) {
-                //FirefoxOptions options = new FirefoxOptions();
-                //options.addArguments("headless");
-                WebDriverManager.firefoxdriver().setup();
-                driver.set(new FirefoxDriver());
-            }
+    public void setUp(String browser) throws MalformedURLException {
 
-            getDriver().manage().window().maximize();
-            getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-            getDriver().get(ConfigReader.getProperty("url"));
+        URL gridUrl = new URL("http://172.26.218.249:4444");
+
+        if (browser.equalsIgnoreCase("chrome")) {
+
+            ChromeOptions options = new ChromeOptions();
+            //options.addArguments("--headless"); // optional
+
+            driver.set(new RemoteWebDriver(gridUrl, options));
+
+        } else if (browser.equalsIgnoreCase("firefox")) {
+
+            FirefoxOptions options = new FirefoxOptions();
+            // options.addArguments("-headless"); // optional
+
+            driver.set(new RemoteWebDriver(gridUrl, options));
         }
+
+        getDriver().manage().window().maximize();
+        getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        getDriver().get(ConfigReader.getProperty("url"));
     }
-    //After Test
+
     @AfterTest(alwaysRun = true)
     public void tearDown() {
         if (driver.get() != null) {
@@ -48,4 +53,3 @@ public class BaseTest {
         }
     }
 }
-
